2209115fed41a8058d5b90bb89056e1a6521e033
Vitaly Baranov <aleshapashkevicha@gmail.com>
2022-02-05 16:46:24 +0300

    add task4

---
+++ task4.js
@@ -0,0 +1,18 @@
+//Выполнить явное преобразование(number, string, boolean)
+
+let value;
+
+//Boolean to number
+value = Number(true);
+console.log(value);
+console.log(typeof value);
+
+//Nubmer to boolean
+value = String(21)
+console.log(value)
+console.log(typeof value);
+
+//String to boolean
+value = Boolean("Vitaly")
+console.log(value);
+console.log(typeof value);
